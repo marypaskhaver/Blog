b0e683794394f6d00b5e2d8ab7f261563ec41e03
Mary Paskhaver <mary.paskhaver@gmail.com>
2021-08-18 18:07:20 -0400

    Use mongoose, make post schema + model to save, render blog posts

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -11,15 +12,31 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
-const posts = [];
-
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/postsDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  content: String
+});
+
+const Post = new mongoose.model("Post", postSchema);
+
 app.get("/", function(req, res) {
-  res.render("home", { startingContent: homeStartingContent, posts: posts } );
+  let posts = Post.find({}, function(err, foundPosts) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("home", { startingContent: homeStartingContent, posts: foundPosts });
+    }
+  });
 });
 
 app.get("/about", function(req, res) {
@@ -34,27 +51,27 @@ app.get("/compose", function(req, res) {
   res.render("compose");
 });
 
-app.get("/posts/:postName", function(req, res) {
-  const requiredTitle = _.lowerCase(req.params.postName);
-
-  posts.forEach(function(post) {
-    const postTitle = _.lowerCase(post.title);
-
-    if (postTitle === requiredTitle) {
-      res.render("post", {title: post.title, content: post.content});
+app.get("/posts/:postId", function(req, res) {
+  Post.findOne({_id: req.params.postId}, function(err, foundPost) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("post", { title: foundPost.title, content: foundPost.content });
     }
   });
 });
 
 app.post("/compose", function(req, res) {
-  const post = {
+  const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody
-  };
+  });
 
-  posts.push(post);
-
-  res.redirect("/");
+  post.save(function(err) {
+    if (!err) {
+      res.redirect("/");
+    }
+  });
 });
 
 app.listen(3000, function() {
